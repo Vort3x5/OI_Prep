2e70fc7d43fbea0016f30490796288aeed24d588
Vort3x5 <Faking.Games857@gmail.com>
2022-01-03 22:20:57 +0100

    na kwadrat powinno wchodzic

--- Logic_Prefs.cpp
+++ Logic_Prefs.cpp
@@ -1,12 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+
+vector <int> arr;
+vector <int> prefs;
+
 void Init()
 {
+    cin >> n;
+    arr.resize(n);
+    prefs.resize(n + 1);
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> arr[i - 1];
+        prefs[i] = prefs[i - 1] ^ arr[i - 1];
+    }
 }
 
 void Solve()
 {
+    int res = 0;
+    for (int i = 1; i <= n; ++i)
+        for (int k = i + 1; k <= n; ++k)
+            res = !(prefs[k] ^ prefs[i - 1]) ? res += (k - i) : res;
+
+    cout << res << '\n';
 }
 
 int main()
