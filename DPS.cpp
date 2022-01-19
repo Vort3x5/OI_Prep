a795176eb7bb34fd0a0b08df61e4ff839668c6b1
Vort3x5 <Faking.Games857@gmail.com>
2022-01-19 19:59:02 +0100

    na przykladowy wchodzi

--- DPS.cpp
+++ DPS.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Query
+struct Qr
 {
     int r, a, b, c;
 };
@@ -11,23 +11,20 @@ struct Node
     int val, add, a, b;
 };
 
-int n, q, l;
+int n, q, l, lfs;
 vector <Node> tree;
-vector <Query> qs;
+vector <Qr> qs;
 
 void Init()
 {
     cin >> n >> q;
-    tree.resize((1 << int((log2(n)) + 1)) + 10);
+    lfs = (1 << int((log2(n)) + 1));
+    tree.resize(2 * lfs);
     qs.resize(q);
-    l = ((1 << int((log2(n)) + 1)) - pow(n, ceil(log2(n))));
+    l = lfs - 1;
     for (int i = 1; i <= n; ++i)
     {
         cin >> tree[i + l].val;
-        for (int node = (i + l); node > 0; node /= 2)
-        {
-            tree[node / 2].val += tree[node].val;
-        }
     }
     for (int i = 0; i < q; ++i)
     {
@@ -35,51 +32,77 @@ void Init()
         if (qs[i].r == 1)
         {
             cin >> qs[i].a >> qs[i].b >> qs[i].c;
-            --qs[i].a, --qs[i].b, --qs[i].c;
         }
         else
         {
             cin >> qs[i].a;
-            --qs[i].a;
         }
     }
-    tree[1].a = 1; tree[1].b = n;
+    tree[1].a = 1, tree[1].b = lfs;
     for (int node = 1; node < (1 + l); ++node)
     {
-        tree[node * 2].a = tree[node].a; 
+        tree[node * 2].a = tree[node].a;
         tree[node * 2].b = (tree[node].a + tree[node].b) / 2;
         
-        tree[(node * 2 + 1)].a = (tree[node].a + tree[node].b) / 2; 
+        tree[(node * 2) + 1].a = (tree[node].a + tree[node].b) / 2 + 1;
         tree[(node * 2) + 1].b = tree[node].b;
     }
 }
 
-int Query(int node, int i, int res = 0)
+bool IsInRange(int v, int node)
 {
-    if (i == (node + l))
+    return ((tree[v].b >= node && node >= tree[v].a) || node == (v - l));
+}
+
+int Query(int node, int v = 1, int res = 0)
+{
+    if (v == (node + l))
     {
-        return (res + tree[node].val);
+        return (res + tree[node + l].val + tree[node + l].add);
     }
     else
     {
-        return (tree[i].b < node ? Query(node, (i * 2) + 1, res + tree[i].add) 
-                : Query(node, i * 2, res + tree[i].add));
+        return (IsInRange(v * 2, node) ? Query(node, v * 2, res + tree[v].add) 
+                : Query(node, (v * 2) + 1, res + tree[v].add));
     }
 }
 
-void Insert(int val, int q_a, int q_b, int i)
-{}
+void Insert(Qr &q, int v = 1)
+{
+    if (q.a > q.b)
+    {
+        return;
+    }
+    else if (q.a == q.b)
+    {
+        tree[q.a + l].add += q.c;
+        return;
+    }
+    if ((tree[v].a == q.a || (v - l) == q.a) && tree[v].b <= q.b)
+    {
+        tree[v].add += q.c;
+        q.a = tree[v].b + 1;
+        return Insert(q, v / 2);
+    }
+    else
+    {
+        Insert(q, (IsInRange(v * 2, q.a) ? v * 2 : (v * 2) + 1));
+    }
+}
 
 void Solve()
 {
-    for (auto qr : qs)
+    for (auto q : qs)
     {
-        if (qr.r == 2)
+        switch (q.r)
         {
-            cout << Query(qr.a, 1) << '\n';
-        }
-        else
-        {
+            case 1:
+                Insert(q);
+                break;
+
+            case 2:
+                cout << Query(q.a) << '\n';
+                break;
         }
     }
 }
