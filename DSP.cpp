295e3c6c723add536ed6d76654e92d9443e32ef6
Vort3x5 <Faking.Games857@gmail.com>
2022-01-24 16:46:56 +0100

    poprawione

--- DSP.cpp
+++ DSP.cpp
@@ -9,11 +9,21 @@ struct Qr
 struct Node
 {
     unsigned long long val; 
-    int a, b;
 };
 
 int n, q, l, lfs;
 vector <Node> tree;
+
+void Insert(Qr qr)
+{
+    int add_b = qr.b;
+    int add_a = tree[qr.a + l].val;
+    for (int node = qr.a + l; node > 0; node /= 2)
+    {
+        tree[node].val -= add_a;
+        tree[node].val += add_b;
+    }
+}
 
 void Init()
 {
@@ -24,51 +34,23 @@ void Init()
     for (int v = 1; v <= n; ++v)
     {
         cin >> tree[v + l].val;
-        for (int node = (v + l) / 2; node > 0; node /= 2)
-        {
-            tree[node].val += tree[v + l].val;
-        }
-    }
-	tree[1].a = 1, tree[1].b = lfs;
-    for (int node = 1; node < (1 + l); ++node)
-    {
-        tree[node * 2].a = tree[node].a;
-        tree[node * 2].b = (tree[node].a + tree[node].b) / 2;
-        
-        tree[(node * 2) + 1].a = (tree[node].a + tree[node].b) / 2 + 1;
-        tree[(node * 2) + 1].b = tree[node].b;
     }
 }
 
-bool IsInRange(long long v, long long node)
+unsigned long long Query(Qr qr, int a = 1, int b = lfs, int v = 1)
 {
-    return ((tree[v].b >= node && node >= tree[v].a) || node == (v - l));
-}
-
-unsigned long long Query(Qr qr, int v = 1)
-{
-    if (tree[v].b < qr.a || tree[v].a > qr.b)
+    if (b < qr.a || a > qr.b)
     {
         return 0;
     }
-    else if (tree[v].a >= qr.a && tree[v].b <= qr.b)
+    else if (a >= qr.a && b <= qr.b)
     {
         return tree[v].val;
     }
     else
     {
-        return Query(qr, v * 2) + Query(qr, (v * 2) + 1);
-    }
-}
-
-void Insert(Qr qr)
-{
-    int add_b = qr.b;
-    int add_a = tree[qr.a + l].val;
-    for (int node = qr.a + l; node > 0; node /= 2)
-    {
-        tree[node].val -= add_a;
-        tree[node].val += add_b;
+        int mid = (a + b) / 2;
+        return Query(qr, a, mid, v * 2) + Query(qr, mid + 1, b, (v * 2) + 1);
     }
 }
 
