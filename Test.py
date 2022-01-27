bdf09a43e5857e8ee4667be2ba47b693f3262ad2
Vort3x5 <Faking.Games857@gmail.com>
2022-01-27 14:56:52 +0100

    szybki swicz

--- Test.py
+++ Test.py
@@ -2,18 +2,24 @@ import subprocess, sys
 
 def Compile():
     try:
-        subprocess.run(["g++", "K0.cpp", "-o", "Given_out/main"], check=True)
+        subprocess.run(["g++", "K0.cpp", "-o", "main"], check=True)
     except subprocess.CalledProcessError:
         print("Compilation of main failed")
         exit(1)
     
     try:
-        subprocess.run(["g++", "Brut.cpp", "-o", "Expected_out/brut"], check=True)
+        subprocess.run(["g++", "Brut.cpp", "-o", "brut"], check=True)
     except subprocess.CalledProcessError:
-        print("Compilation of main failed")
+        print("Compilation of brut failed")
         exit(1)
 
-def Run():
+def MakeCases(N):
+    subprocess.run(["python3", "Gen.py", str(N)])
+    for i in range (N):
+        pass
+
+def CmpRes():
     pass
 
 Compile()
+# MakeCases(sys.argv[1])
