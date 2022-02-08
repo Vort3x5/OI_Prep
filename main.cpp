0f74a999b4969a5c7ee5f9efca8ced448d86dbd1
Vort3x5 <Faking.Games857@gmail.com>
2022-02-08 16:54:04 +0100

    wszystko po za query

---
+++ main.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+#define nol nums_of_lfs
+
+using namespace std;
+
+struct lf
+{
+    int indx, val;
+};
+
+bool operator<(lf l, lf r)
+    { return (l.val != r.val ? l.val < r.val : l.indx < r.indx); }
+
+struct Node
+{
+    set <lf> lfs;
+};
+
+struct Query
+{
+    int type, a, b;
+};
+
+int n, q, nums_of_lfs;
+
+vector <int> arr;
+vector <Node> tree;
+
+void FillTree(int lvl_l = nol / 2, int node = nol / 2)
+{
+    tree[node].lfs = tree[node * 2].lfs;
+    tree[node].lfs.insert(
+            tree[(node * 2 ) + 1].lfs.begin(), tree[(node * 2) + 1].lfs.end());
+
+    if (node == 1)
+    {
+        return;
+    }
+    else
+    {
+        FillTree(
+                node - lvl_l == lvl_l - 1 ? lvl_l / 2 : lvl_l, 
+                node - lvl_l == lvl_l - 1 ? lvl_l / 2 : node + 1);
+    }
+}
+
+void Init()
+{
+    cin >> n >> q;
+    arr.resize(n);
+    nol = (1 << int((log2(n)) + 1));
+    tree.resize(2 * nol);
+    for (auto &v : arr)
+    {
+        static int node = 0;
+        cin >> v;
+        tree[node + nol].lfs.insert({node, v});
+        ++node;
+    }
+    FillTree();
+}
+
+void Insert(int node, int val)
+{
+    static int v = node + nol;
+    auto to_del = tree[v].lfs.find({node, arr[node]});
+    tree[v].lfs.erase(to_del);
+    tree[v].lfs.insert({node, val});
+    if (v == 1)
+    {
+        return;
+    }
+    v /= 2;
+    Insert(node, val);
+}
+
+int Qr(int a, int b, int this_a, int this_b)
+{
+    static set <lf> range;
+}
+
+void Solve()
+{
+    for (int i = 0; i < q; ++i)
+    {
+        Query qr;
+        cin >> qr.type >> qr.a >> qr.b;
+        switch (qr.type)
+        {
+            case 1:
+                Insert(qr.a - 1, qr.b);
+                break;
+
+            case 2:
+                cout << Qr(qr.a - 1, qr.b) << '\n';
+                break;
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    Init();
+    Solve();
+
+    return 0;
+}
