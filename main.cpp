7460db6b68d21b65cb77249ec422d038f40a9709
Vort3x5 <Faking.Games857@gmail.com>
2022-02-15 14:07:09 +0100

    zly wynik

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ typedef vector <int> v_i;
 
 struct Node
 {
-    int val;
+    int psoms, sum, pref, suf;
 };
 
 struct Query
@@ -19,9 +19,33 @@ struct Query
     int type, x, y;
 };
 
-int N, M, lfs;
+int N, M, lfs, min_v = -10010;
 
 vector <Node> tree;
+
+void Update(int x, Node &parent, Node l, Node r, bool is_child = false)
+{
+    if (is_child)
+    {
+        parent.pref = parent.suf = parent.psoms = parent.sum = x;
+    }
+    else
+    {
+        parent.psoms = max(max(l.psoms, r.psoms), l.suf + r.pref);
+        parent.suf = max(r.suf, r.sum + l.suf);
+        parent.pref = max(l.pref, l.sum + r.pref);
+        parent.sum = l.sum + r.sum;
+    }
+}
+
+void Insert(int node, int x)
+{
+    while (node > 0)
+    {
+        Update(x, tree[node], tree[node * 2], tree[(node * 2) + 1]);
+        node /= 2;
+    }
+}
 
 void Init()
 {
@@ -30,27 +54,31 @@ void Init()
     tree.resize(2 * lfs);
     for (int node = 0; node < N; ++node)
     {
-        cin >> tree[lfs + node].val;
-        for (int v = lfs + node; v >= 1; v /= 2)
-        {
-            tree[v].val += tree[lfs + node].val;
-        }
+        int x;
+        cin >> x;
+        Update(x, tree[node + lfs], tree[node + lfs], tree[node + lfs], true);
+        Insert((node + lfs) / 2, x);
     }
     cin >> M;
 }
 
-void Insert(int node, int prev_x, int x)
+Node Qr(Query q, int a = 1, int b = N, int node = 1)
 {
-    tree[node].val += (x - prev_x);
-    if (node == 1)
+    static Node res;
+    if (b < q.x || a > q.y)
     {
-        return;
+        return { min_v, 0, 0, 0 };
     }
-    Insert(node /= 2, prev_x, x);
-}
-
-int Qr()
-{
+    else if (a >= q.x && b <= q.y)
+    {
+        return tree[node];
+    }
+    else
+    {
+        int mid = (a + b) / 2;
+        Update(0, res, Qr(q, a, mid, node * 2), Qr(q, mid + 1, b, (node * 2) + 1));
+        return res;
+    }
 }
 
 void Solve()
@@ -62,11 +90,11 @@ void Solve()
         switch (q.type)
         {
             case 0:
-                Insert(q.x, tree[lfs + q.x].val, q.y);
+                Insert(q.x, q.y + lfs - 1);
                 break;
             
             case 1:
-                // cout << Qr() << '\n';
+                cout << Qr(q).psoms << '\n';
                 break;
         }
     }
