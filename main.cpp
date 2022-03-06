c23638861f07f73ad6469cc28ae4d30dacb29eb4
Vort3x5 <Faking.Games857@gmail.com>
2022-03-06 13:56:14 +0100

    skonczone, setka jest

--- main.cpp
+++ main.cpp
@@ -11,31 +11,32 @@ typedef vector <int> v_i;
 
 struct Node
 {
-    int sum, lazy;
+    ll sum, lazy;
 };
 
 struct Query
 {
-    int type, p, q, v;
+    ll type, p, q, v;
 };
 
-int T, N, C, lfs, l;
+ll T, N, C, lfs, l;
 
 vector <Node> tree;
 
 void Init()
 {
     cin >> N >> C;
-    lfs = (1 << int((log2(N)) + 1));
+    lfs = (1 << ll((log2(N)) + 1));
+    tree.clear();
     tree.resize(2 * lfs);
     l = lfs - 1;
 }
 
-void GoDown(int &mid, int &abl, int &abr, int a, int b, int v)
+void GoDown(ll &mid, ll a, ll b, ll v)
 {
     mid = (a + b) / 2;
-    abl = mid - a + 1;
-    abr = b - mid;
+    ll abl = mid - a + 1;
+    ll abr = b - mid;
     tree[v * 2].lazy += tree[v].lazy;
     tree[(v * 2) + 1].lazy += tree[v].lazy;
     tree[v * 2].sum += tree[v].lazy * abl;
@@ -43,7 +44,7 @@ void GoDown(int &mid, int &abl, int &abr, int a, int b, int v)
     tree[v].lazy = 0;
 }
 
-void Insert(Query q, int a = 1, int b = lfs, int v = 1)
+void Insert(Query q, ll a = 1, ll b = lfs, ll v = 1)
 {
     if (a > q.q || b < q.p)
     {
@@ -56,15 +57,15 @@ void Insert(Query q, int a = 1, int b = lfs, int v = 1)
     }
     else
     {
-        int mid, abl, abr;
-        GoDown(mid, abl, abr, a, b, v);
+        ll mid;
+        GoDown(mid, a, b, v);
         Insert(q, a, mid, v * 2);
         Insert(q, mid + 1, b, (v * 2) + 1);
         tree[v].sum = tree[v * 2].sum + tree[(v * 2) + 1].sum;
     }
 }
 
-int Qr(Query q, int a = 1, int b = lfs, int v = 1)
+ll Qr(Query q, ll a = 1, ll b = lfs, ll v = 1)
 {
     if (a > q.q || b < q.p)
     {
@@ -76,8 +77,8 @@ int Qr(Query q, int a = 1, int b = lfs, int v = 1)
     }
     else
     {
-        int mid, abl, abr;
-        GoDown(mid, abl, abr, a, b, v);
+        ll mid, abl, abr;
+        GoDown(mid, a, b, v);
         return Qr(q, a, mid, v * 2) +
         Qr(q, mid + 1, b, (v * 2) + 1);
     }
@@ -86,10 +87,10 @@ int Qr(Query q, int a = 1, int b = lfs, int v = 1)
 void Solve()
 {
     cin >> T;
-    for (int i = 0; i < T; ++i)
+    for (ll i = 0; i < T; ++i)
     {
         Init();
-        for (int j = 0; j < C; ++j)
+        for (ll j = 0; j < C; ++j)
         {
             Query q;
             cin >> q.type >> q.p >> q.q;
