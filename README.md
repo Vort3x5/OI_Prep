f78e40cfb13e060a7416416f486d424d10bee99f
Vort3x5 <Faking.Games857@gmail.com>
2022-04-04 16:33:52 +0200

    p

---
+++ README.md
@@ -0,0 +1,1 @@
+Problem: https://szkopul.edu.pl/problemset/problem/aKKSmtjWTtDOEHDqnmQ3-eAA/site/?key=statement
--- Src/main.cpp
+++ Src/main.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include "../../pch.h"
 
 #define pb push_back
 #define pf push_front
@@ -9,8 +9,20 @@ typedef long long ll;
 typedef pair <int, int> p_i;
 typedef vector <int> v_i;
 
+int n, m;
+
+vector <vector <int>> graph;
+
 void Init()
 {
+    scanf("%d%d", &n, &m);
+    graph.resize(n + 10);
+    for (int e = 0; e < m; ++e)
+    {
+        int src, dest;
+        scanf("%d%d", &src, &dest);
+        graph[src].pb(dest);
+    }
 }
 
 void Solve()
@@ -19,9 +31,6 @@ void Solve()
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-
     Init();
     Solve();
 
