928a604033c22e08404dc2bc798287724fc98ee1
Vort3x5 <Faking.Games857@gmail.com>
2022-04-09 11:22:04 +0200

    wstepnie zaklepane

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,64 +1,120 @@
-#include <vector>
-#include <iostream>
+#include "../../pch.h"
+
+#define pb push_back
+#define pf push_front
 
 using namespace std;
 
-const int N = 100010;
+typedef long long ll;
+typedef pair <int, int> p_i;
+typedef vector <int> v_i;
 
-int n, q;
-vector <int> T[N];
-vector <int> depth;
-vector <pair <int, int>> io; 
-vector <int> l;
-vector <int> pos;
-
-void DFS (int r, int p, int odl, int &t) 
+struct Edge
 {
-    l.push_back(r);
-    pos[r] = l.size() - 1;
-    ++t;
-    io[r].first = t;
-    depth[r] = odl;
-    for (auto neighbour : T[r])
+    vector <int> childern;
+    int pos;
+};
+
+struct Node
+{
+    int val;
+};
+
+int t, N, Q, lfs, l, root, mx_val = 1010;
+
+vector <Edge> graph;
+vector <int> found;
+vector <Node> tree;
+
+void Init()
+{
+    scanf("%d", &N);
+    graph.resize(N + 10);
+    graph.clear();
+    found.clear();
+    tree.clear();
+    for (int v = 1; v <= N; ++v)
     {
-        if (neighbour != p)
+        int M;
+        scanf("%d", &M);
+        root += !root * v;
+        for (int j = 0; j < M; ++j)
         {
-            DFS(neighbour, r, odl + 1, t);
-            l.push_back(r);
+            int q;
+            scanf("%d", &q);
+            graph[v].childern.pb(q);
         }
     }
-    ++t;
-    io[r].second = t;
 }
 
-bool isAnc (int a, int b) 
+void Insert(int node, int v)
 {
-    return (io[a].first <= io[b].first && io[a].second <= io[b].second);
+    tree[v].val = node;
+    while(v)
+    {
+        tree[v].val = min(tree[v * 2].val, tree[(v * 2) + 1].val);
+        v /= 2;
+    }
 }
 
-int LCA (int a, int b) 
+void BuildTree()
 {
-    if (isAnc(a, b))
-        return a;
-    if (isAnc(b, a))
-        return b;
+    lfs = 1 << int(log2(found.size()) + 1);
+    l = lfs - 1;
+    for (int v = 1; v <= found.size(); ++v)
+        Insert(found[v], v + l);
+}
+
+void Dfs(int v, int i = 1)
+{
+    found.pb(graph[v].pos);
+    graph[v].pos += !graph[v].pos * i;
+    for (auto node : graph[v].childern)
+    {
+        Dfs(node, ++i);
+        found.pb(graph[v].pos);
+    }
+}
+
+int Query(int q_a, int q_b, int a = 1, int b = lfs, int v = 1)
+{
+    if (b < q_a || a > q_b)
+        return mx_val;
+    else if (q_a <= a || b <= q_b)
+        return tree[v].val;
+    else
+    {
+        int mid = (a + b) / 2;
+        return min(
+                Query(q_a, q_b, a, mid, v * 2), 
+                Query(q_a, q_b, mid + 1, b, (v * 2) + 1));
+    }
+}
+
+void Solve()
+{
+    scanf("%d", &t);
+    for (int i = 1; i <= t; ++i)
+    {
+        Init();
+        scanf("%d", &Q);
+        Dfs(root);
+        printf("Case %d:\n", i);
+        for (int i = 0; i < Q; ++i)
+        {
+            int v, q;
+            scanf("%d%d", &v, &q);
+            printf(
+                "%d\n", Query(
+                            graph[v].pos <= graph[q].pos ? graph[v].pos : graph[q].pos,
+                            graph[v].pos <= graph[q].pos ? graph[q].pos : graph[v].pos));
+        }
+    }
 }
 
 int main()
 {
-    cin >> n >> q;
-
-    for (int i = 1; i < n; i++) {
-
-        int a, b;
-        cin >> a >> b;
-        T[a].push_back(b);
-        T[b].push_back(a);
-    }
-    int k;
-
-    DFS(1, 1, 0, k);
-
+    Solve();
 
     return 0;
 }
