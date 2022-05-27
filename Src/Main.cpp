4c33e69ef8d839a55e8d108a1501890d73eb7ae8
Vort3x5 <Faking.Games857@gmail.com>
2022-05-27 14:08:27 +0200

    narazie mam to

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -12,107 +12,77 @@ typedef vector <int> v_i;
 struct Edge
 {
     vector <int> deston;
+    int in, out;
     bool vis;
 };
 
 int n, m;
 
 vector <Edge> graph;
-vector <vector <int>> rev_g;
-stack <int> topo_sort;
-vector <int> scc;
+deque <int> eul_path;
+stack <int> start;
 
 void Init()
 {
     scanf("%d%d", &n, &m);
-    graph.resize((m * 2) + 10);
-    rev_g.resize((m * 2) + 10);
-    scc.resize((m * 2) + 10);
-    for (int i = 0; i < n; ++i)
+    graph.resize(n + 10);
+    for (int i = 0; i < m; ++i)
     {
-        char fst[3], snd[3];
-        int p, q;
-        scanf("%s%d%s%d", fst, &p, snd, &q);
-
-        if (fst[0] == '+' && snd[0] == '+')
-        {
-            graph[m + p].deston.pb(q);
-            graph[m + q].deston.pb(p);
-
-            rev_g[q].pb(m + p);
-            rev_g[p].pb(m + q);
-        }
-        else if (fst[0] == '+' && snd[0] == '-')
-        {
-            graph[m + p].deston.pb(m + q);
-            graph[q].deston.pb(p);
-            
-            rev_g[m + q].pb(m + p);
-            rev_g[p].pb(q);
-        }
-        else if (fst[0] == '-' && snd[0] == '+')
-        {
-            graph[p].deston.pb(q);
-            graph[m + q].deston.pb(m + p);
-            
-            rev_g[q].pb(p);
-            rev_g[m + p].pb(m + q);
-        }
-        else if (fst[0] == '-' && snd[0] == '-')
-        {
-            graph[p].deston.pb(m + q);
-            graph[q].deston.pb(m + p);
-            
-            rev_g[m + q].pb(p);
-            rev_g[m + p].pb(q);
-        }
+        int v, u;
+        scanf("%d%d", &v, &u);
+        graph[v].deston.pb(u);
     }
 }
 
-void TopoSort(int v)
+void CountDegrees(int v)
 {
     graph[v].vis = true;
     for (auto node : graph[v].deston)
+    {
+        ++graph[v].out;
+        ++graph[node].in;
         if (!graph[node].vis)
-            TopoSort(node);
-    topo_sort.push(v);
+            CountDegrees(node);
+    }
 }
 
-void InitScc(int v, int i)
+void FindEulPath(int v)
 {
     graph[v].vis = false;
-    scc[v] = i;
-    for (auto node : rev_g[v])
-        if (graph[node].vis)
-            InitScc(node, i);
+    for (int node = 0; node < graph[v].deston.size(); ++node)
+    {
+        if (graph[graph[v].deston[node]].vis)
+        {
+            --graph[v].out;
+            FindEulPath(graph[v].deston[node]);
+        }
+    }
+    eul_path.push_front(v);
 }
 
 void Solve()
 {
-    for (int v = 1; v <= (m * 2); ++v)
+    for (int v = 1; v <= n; ++v)
         if (!graph[v].vis)
-            TopoSort(v);
-    for (int i = 1; !topo_sort.empty();)
+            CountDegrees(v);
+    for (int v = 1; v < n; ++v)
     {
-        if (graph[topo_sort.top()].vis)
-            InitScc(topo_sort.top(), i++);
-        topo_sort.pop();
+        if ((graph[v].out - graph[v].in) == 1)
+            start.push(v);
     }
-    for (int v = 1; v <= m; ++v)
+    while (!start.empty())
     {
-        if (scc[v] == scc[v + m])
+        if (graph[start.top()].vis)
+            FindEulPath(start.top());
+        else
         {
             printf("IMPOSSIBLE\n");
             return;
         }
+        start.pop();
     }
-    for (int v = 1; v <= m; ++v)
-    {
-        if (scc[v] < scc[v + m])
-            printf("- ");
-        else
-            printf("+ ");
-    }
+    for (auto res : eul_path)
+        printf("%d ", res);
     printf("\n");
 }
 
