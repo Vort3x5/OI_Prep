60fd56820e3cf2b34b79553ac55610da85daf917
Vort3x5 <Faking.Games857@gmail.com>
2022-06-08 17:57:43 +0200

    zaklepane na lekcji

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -11,126 +11,110 @@ typedef vector <int> v_i;
 
 struct Edge
 {
-    v_i deston, id;
-    int in, out;
+    int deep, stc = 1, heavy = -1, parent, pos, head, w;
+    v_i deston;
+    v_i dist;
     bool vis;
 };
 
-int n, m, start, s_end;
+int n, lfs, l, pos;
 
 vector <Edge> graph;
-deque <int> eul_path;
-bitset <2000010> e_vis;
+vector <int> tree;
 
 void Init()
 {
-    scanf("%d%d", &n, &m);
+    scanf("%d", &n);
     graph.resize(n + 10);
-    for (int i = 1; i <= m; ++i)
+    lfs = 1 << int(log2(n - 1) + 1);
+    l = lfs - 1;
+    tree.resize(2 * lfs);
+    for (int i = 1; i < n; ++i)
     {
-        int v, u;
-        scanf("%d%d", &v, &u);
+        int v, w, u;
         graph[v].deston.pb(u);
-        graph[v].id.pb(i);
+        graph[v].dist.pb(w);
+        
+        graph[u].deston.pb(v);
+        graph[u].dist.pb(w);
     }
 }
 
-void CountDegrees(int v)
+void Insert(int x, int v)
+{
+    while (v)
+    {
+        tree[v] += x;
+        v /= 2;
+    }
+}
+
+int Query(int q_a, int q_b, int t_a = 1, int t_b = lfs, int v = 1)
+{
+    if (t_a > q_b || t_b < q_a)
+        return 0;
+    else if (t_a >= q_a && t_b <= q_b)
+        return tree[v];
+    else
+    {
+        int mid = (t_a + t_b) / 2;
+        return Query(q_a, q_b, t_a, mid, v * 2) + Query(q_a, q_b, mid + 1, t_b, (v * 2) + 1);
+    }
+}
+
+int Dfs(int v, int p, int w, int d = 1)
 {
     graph[v].vis = true;
-    for (auto node : graph[v].deston)
+    graph[v].deep = d;
+    graph[v].parent = p;
+    graph[v].w = w;
+    for (int node = 0; node < graph[v].deston.size(); ++node)
     {
-        ++graph[v].out;
-        ++graph[node].in;
-        if (!graph[node].vis)
-            CountDegrees(node);
-    }
-}
-
-void FindEulPath(int v)
-{
-    for (int node = 0; node < graph[v].id.size(); ++node)
-    {
-        if (!e_vis[graph[v].id[node]])
+        if (!graph[graph[v].deston[node]].vis)
         {
-            e_vis[graph[v].id[node]] = true;
-            FindEulPath(graph[v].deston[node]);
+            graph[v].stc += Dfs(graph[v].deston[node], v, graph[v].dist[node], d + 1);
         }
     }
-    eul_path.push_front(v);
+    for (int node : graph[v].deston)
+    {
+        if (node != p)
+            graph[v].heavy = graph[v].heavy == -1 ? node : 
+                graph[graph[v].heavy].stc > graph[node].stc ? graph[v].heavy : node;
+    }
+    return graph[v].stc;
 }
 
-bool WereVisited()
+void Decompose(int h, int v)
 {
-    for (int v = 1; v <= n; ++v)
+    graph[v].pos = pos++;
+    Insert(pos + l - 1, graph[v].w);
+    graph[v].head = h;
+    if (graph[v].heavy == -1)
+        return;
+    Decompose(h, graph[v].heavy);
+    for (int node : graph[v].deston)
     {
-        for (auto edge : graph[v].id)
-            if (!e_vis[edge])
-                return false;
+        if (node != graph[v].heavy && node != graph[v].parent)
+            Decompose(node, node);
     }
-    return true;
+}
+
+int HldQr(int a, int b)
+{
+    int res = 0;
+    while (graph[a].head != graph[b].head)
+    {
+        if (graph[graph[a].head].deep < graph[graph[b].head].deep)
+            swap(a, b);
+        res += Query(graph[graph[a].head].pos, graph[a].pos);
+        a = graph[graph[a].head].parent;
+    }
+    if (a != b)
+        res += Query(graph[a].pos < graph[b].pos ? graph[a].pos : graph[b].pos, graph[a].pos > graph[b].pos ? graph[a].pos - 1 : graph[b].pos - 1);
 }
 
 void Solve()
 {
-    for (int v = 1; v <= n; ++v)
-        if (!graph[v].vis)
-            CountDegrees(v);
-    
-    bool cycle = true;
-    for (int v = 1; v <= n; ++v)
-    {
-        switch (graph[v].out - graph[v].in)
-        {
-            case 0:
-                break;
-
-            case 1:
-                cycle = false;
-                if (!start)
-                    start = v;
-                else
-                {
-                    printf("IMPOSSIBLE\n");
-                    return;
-                }
-                break;
-            
-            case -1:
-                cycle = false;
-                if (!s_end)
-                    s_end = v;
-                else
-                {
-                    printf("IMPOSSIBLE\n");
-                    return;
-                }
-                break;
-            
-            default:
-                cycle = false;
-                break;
-        }
-    }
-    if (!cycle && (!start || !s_end))
-    {
-        printf("IMPOSSIBLE\n");
-        return;
-    }
-
-    if (cycle)
-        start = 1;
-    FindEulPath(start);
-    
-    if (!WereVisited() || eul_path.back() != n)
-    {
-        printf("IMPOSSIBLE\n");
-        return;
-    }
-    
-    for (auto res : eul_path)
-        printf("%d ", res);
-    printf("\n");
 }
 
 int main()
