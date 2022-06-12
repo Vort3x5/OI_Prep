2ad6022baf4e1488924a3165d2fbbca46432555c
Vort3x5 <Faking.Games857@gmail.com>
2022-06-12 18:03:51 +0200

    pierwsze kopiowanko

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -11,27 +11,27 @@ typedef vector <int> v_i;
 
 struct Edge
 {
-    int deep, stc = 1, heavy = -1, parent, pos, head, w;
-    v_i deston;
-    v_i dist;
+    v_i deston, dist;
+    int deep, parent, pos, head, w, stc = 1, heavy = -1;
     bool vis;
 };
 
-int n, lfs, l, pos;
+int t, N, lfs, l, pos;
 
 vector <Edge> graph;
 vector <int> tree;
 
 void Init()
 {
-    scanf("%d", &n);
-    graph.resize(n + 10);
-    lfs = 1 << int(log2(n - 1) + 1);
+    scanf("%d", &N);
+    graph.resize(N + 10);
+    lfs = 1 << int(log2(N - 1) + 1);
     l = lfs - 1;
-    tree.resize(2 * lfs);
-    for (int i = 1; i < n; ++i)
+    tree.resize(lfs * 2);
+    for (int i = 0; i < (N - 1); ++i)
     {
-        int v, w, u;
+        int v, u, w;
+        scanf("%d%d%d", &v, &u, &w);
         graph[v].deston.pb(u);
         graph[v].dist.pb(w);
         
@@ -42,11 +42,9 @@ void Init()
 
 void Insert(int x, int v)
 {
-    while (v)
-    {
-        tree[v] += x;
-        v /= 2;
-    }
+    tree[v] = x;
+    while (v /= 2)
+        tree[v] = tree[v * 2] + tree[(v * 2) + 1];
 }
 
 int Query(int q_a, int q_b, int t_a = 1, int t_b = lfs, int v = 1)
@@ -58,63 +56,30 @@ int Query(int q_a, int q_b, int t_a = 1, int t_b = lfs, int v = 1)
     else
     {
         int mid = (t_a + t_b) / 2;
-        return Query(q_a, q_b, t_a, mid, v * 2) + Query(q_a, q_b, mid + 1, t_b, (v * 2) + 1);
+        return max(Query(q_a, q_b, t_a, mid, v * 2), Query(q_a, q_b, mid + 1, t_b, (v * 2) + 1));
     }
-}
-
-int Dfs(int v, int p, int w, int d = 1)
-{
-    graph[v].vis = true;
-    graph[v].deep = d;
-    graph[v].parent = p;
-    graph[v].w = w;
-    for (int node = 0; node < graph[v].deston.size(); ++node)
-    {
-        if (!graph[graph[v].deston[node]].vis)
-        {
-            graph[v].stc += Dfs(graph[v].deston[node], v, graph[v].dist[node], d + 1);
-        }
-    }
-    for (int node : graph[v].deston)
-    {
-        if (node != p)
-            graph[v].heavy = graph[v].heavy == -1 ? node : 
-                graph[graph[v].heavy].stc > graph[node].stc ? graph[v].heavy : node;
-    }
-    return graph[v].stc;
-}
-
-void Decompose(int h, int v)
-{
-    graph[v].pos = pos++;
-    Insert(pos + l - 1, graph[v].w);
-    graph[v].head = h;
-    if (graph[v].heavy == -1)
-        return;
-    Decompose(h, graph[v].heavy);
-    for (int node : graph[v].deston)
-    {
-        if (node != graph[v].heavy && node != graph[v].parent)
-            Decompose(node, node);
-    }
-}
-
-int HldQr(int a, int b)
-{
-    int res = 0;
-    while (graph[a].head != graph[b].head)
-    {
-        if (graph[graph[a].head].deep < graph[graph[b].head].deep)
-            swap(a, b);
-        res += Query(graph[graph[a].head].pos, graph[a].pos);
-        a = graph[graph[a].head].parent;
-    }
-    if (a != b)
-        res += Query(graph[a].pos < graph[b].pos ? graph[a].pos : graph[b].pos, graph[a].pos > graph[b].pos ? graph[a].pos - 1 : graph[b].pos - 1);
 }
 
 void Solve()
 {
+    scanf("%d", &t);
+    for (int i = 0; i < t; ++i)
+    {
+        Init();
+        string qr;
+        int a, b;
+        while (qr != "DONE")
+        {
+            cin >> qr;
+            scanf("%d%d", &a, &b);
+            if (qr == "QUERY")
+            {
+            }
+            else if (qr == "CHANGE")
+            {
+            }
+        }
+    }
 }
 
 int main()
