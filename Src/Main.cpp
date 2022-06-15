583b71a7483d5a7c8d85ef88bea6ae71c006633d
Vort3x5 <Faking.Games857@gmail.com>
2022-06-15 21:48:02 +0200

    p

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -11,25 +11,68 @@ typedef vector <int> v_i;
 
 struct Edge
 {
-    v_i deston;
+    int dest;
+    bool tres;
+};
+
+struct Node
+{
+    vector <Edge> deston;
+    bool vis;
+    int deep;
 };
 
 int n, m, src, dest;
 
-vector <Edge> graph;
+vector <Node> graph;
+vector <Node> dfs_g;
 
 void Init()
 {
     scanf("%d%d", &n, &m);
     graph.resize(n + 10);
+    dfs_g.resize(n + 10);
     for (int i = 0; i < m; ++i)
     {
+        int v, u, art;
+        scanf("%d%d%d", &v, &u, &art);
+        
+        graph[v].deston.pb({u, (bool)art});
+        graph[u].deston.pb({v, (bool)art});
     }
     scanf("%d%d", &src, &dest);
 }
 
+void CountDeep(int v = 1, int d = 1)
+{
+    dfs_g[v].vis = false;
+    dfs_g[v].deep = d;
+    for (auto node : dfs_g[v].deston)
+    {
+        if (dfs_g[node.dest].vis)
+            CountDeep(node.dest, d + 1);
+    }
+}
+
+void BuildDfsTree(int v = 1)
+{
+    graph[v].vis = true;
+    for (auto node : graph[v].deston)
+    {
+        if (!graph[node.dest].vis)
+        {
+            dfs_g[v].deston.pb(node);
+            BuildDfsTree(node.dest);
+        }
+        else
+            dfs_g[node.dest].deston.pb({v, graph[v].deston[node.dest].tres});
+    }
+}
+
 void Solve()
 {
+    BuildDfsTree();
+    CountDeep();
 }
 
 int main()
