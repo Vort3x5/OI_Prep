7f49acff23c50fe312730b31a25b87e233322118
Vort3x5 <Faking.Games857@gmail.com>
2022-06-25 14:29:06 +0200

    calosc z segfaultem

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -16,7 +16,7 @@ int n, dp[100010][2];
 struct Edge
 {
     v_i deston;
-    bool vis;
+    bool vis[2];
 };
 
 vector <Edge> graph;
@@ -30,23 +30,26 @@ void Init()
         int v, u;
         scanf("%d%d", &v, &u);
         graph[v].deston.pb(u);
+        graph[u].deston.pb(v);
     }
 }
 
-int Dfs(int v = 1, int status = 0)
+int Dfs(int status, int iter = 0, int v = 1)
 {
-    graph[v].vis = true;
+    graph[v].vis[iter] = true;
     for (int node : graph[v].deston)
     {
-        if (!graph[node].vis)
-            dp[v][0] += Dfs(node, 1), dp[v][1] += Dfs(node, 0);
+        if (!graph[node].vis[iter])
+            dp[v][status] += Dfs(!status, node);
     }
-    return 0;
+    return dp[v][status] + status;
 }
 
 void Solve()
 {
-    Dfs();
+    Dfs(0);
+    Dfs(1, 1);
+    printf("%d\n", min(dp[1][0], dp[1][1]));
 }
 
 int main()
