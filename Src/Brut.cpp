1efb456fe97770d57dc59c0d25f3be2d0b336479
Vort3x5 <Faking.Games857@gmail.com>
2022-07-19 12:20:21 +0200

    przykladowy dziala, reszta nie

---
+++ Src/Brut.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define pf push_front
+
+using namespace std;
+
+typedef long long ll;
+typedef pair <int, int> p_i;
+typedef vector <int> v_i;
+
+int n, t;
+string s1, s2;
+
+void Init()
+{
+    scanf("%d", &n);
+    cin >> s1 >> s2;
+}
+
+void Solve()
+{
+}
+
+int main()
+{
+    Init();
+    Solve();
+
+    return 0;
+}
