dd9e6b044c930b4a8fb5ecb64bf8981eded22127
Vort3x5 <Faking.Games857@gmail.com>
2022-08-15 12:56:26 +0200

    Edu15 A

--- Edu15/A.cpp
+++ Edu15/A.cpp
@@ -12,30 +12,32 @@ typedef vector <int> v_i;
 int n, res;
 
 v_i arr;
-vector <ll> dp;
 
 void Init()
 {
     cin >> n;
     arr.resize(n);
-    dp = vector <ll> (n + 1, INF);
-    dp[0] = -INF;
     for (int i = 0; i < n; ++i)
         cin >> arr[i];
 }
 
 void Solve()
 {
+    int prev = 0, curr = 0;
     for (int i = 0; i < n; ++i)
     {
-        int j = upper_bound(dp.begin(), dp.end(), arr[i]) - dp.begin();
-        if (dp[j - 1] < arr[i] && arr[i] < dp[j])
-            dp[j] = arr[i];
+        if (arr[i] > prev)
+        {
+            ++curr;
+            res = max(res, curr);
+        }
+        else
+        {
+            res = max(res, curr);
+            curr = 1;
+        }
+        prev = arr[i];
     }
-    ll res = 0;
-    for (int i = 0; i <= n; ++i)
-        if (dp[i] < INF)
-            res = i;
     cout << res << '\n';
 }
 
