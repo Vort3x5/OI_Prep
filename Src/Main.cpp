634605bf09576ffa6a99638957bd971aab6c08ba
Vort3x5 <Faking.Games857@gmail.com>
2022-08-18 14:06:42 +0200

    Setup

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -13,15 +13,16 @@ typedef vector <int> v_i;
 int log_of_l;
 string s, sub1, sub2;
 
-vector <v_i> suf;
+vector <v_i> kmr;
+vector <p_i> id;
 
 void Init()
 {
     cin >> s >> sub1 >> sub2;
     log_of_l = int(log2(s.size()) + 1);
-    suf.resize(log_of_l);
-    id.resize(300);
-    for (v_i &vi : suf)
+    kmr.resize(log_of_l + 1);
+    id.resize(s.size() + 1);
+    for (v_i &vi : kmr)
         vi.resize(s.size() + 1);
 }
 
@@ -29,7 +30,7 @@ void KMR()
 {
     int j = 1;
     for (int i = 0; i < s.size(); ++i)
-        suf[0][i] = s[i] - 'a' + 1;
+        kmr[1][i] = s[i] - 'a' + 1;
 }
 
 void Solve()
