b819b2be355643e70f58ea97564508f0f8b85ca3
Vort3x5 <Faking.Games857@gmail.com>
2022-08-28 13:51:06 +0200

    cp

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -13,24 +13,49 @@ typedef vector <int> v_i;
 int log_of_l;
 string s, sub1, sub2;
 
-vector <v_i> kmr;
 vector <p_i> id;
+vector <v_i> kmr;
 
 void Init()
 {
     cin >> s >> sub1 >> sub2;
     log_of_l = int(log2(s.size()) + 1);
-    kmr.resize(log_of_l + 1);
     id.resize(s.size() + 1);
-    for (v_i &vi : kmr)
-        vi.resize(s.size() + 1);
 }
 
 void KMR()
 {
-    int j = 1;
-    for (int i = 0; i < s.size(); ++i)
-        kmr[1][i] = s[i] - 'a' + 1;
+    pair <int , pair <int, int> > pomoc[s.size() + 2];
+    int r = log(s.size()) + 1, pot = 1;
+    for (int i = 0; i < s.size(); i ++)
+        kmr[i][0] = (s[i] - 'a') + 1;
+    for (int x = 1;x <= r; x ++) {
+        for(int i = 0;i < s.size(); i ++) {
+            if (i+pot >= s.size())
+                pomoc[i] = make_pair(
+                    kmr[i][x-1],
+                    make_pair(-1, i));
+            else
+                pomoc[i] = make_pair(
+                    kmr[i][x-1],
+                    make_pair(kmr[i+pot][x-1], i));
+        }
+        sort(pomoc,pomoc+s.size());
+        pair <int, int> pom = make_pair(-1,-1);
+        int ile = -1;
+        for(int i = 0;i < s.size(); i ++) {
+            if(pom.first != pomoc[i].first
+                || pom.second != pomoc[i].second.first
+            ) {
+                pom = make_pair(
+                    pomoc[i].first,
+                    pomoc[i].second.first);
+                    ++ile;
+            }
+            kmr[pomoc[i].second.second][x] = ile;
+        }
+        pot *= 2;
+    }
 }
 
 void Solve()
