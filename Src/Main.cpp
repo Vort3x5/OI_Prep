759f7f4f2bd4e7095c2b09846e259bb79146e07b
Vort3x5 <Faking.Games857@gmail.com>
2022-10-08 12:59:58 +0200

    Setup

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -16,74 +16,19 @@ typedef pair <s64, s64> p_ll;
 typedef vector <int> v_i;
 typedef vector <s64> v_ll;
 
-s32 T, N;
+int t, N;
 
-pair <bool, s32> sieve[20000010];
-int sigma[20000010];
-bitset <20000010> res;
-
-inline void Init()
+void Init()
 {
     cin >> N;
 }
 
-void EraSieve()
-{
-    for (int i = 2; i <= 20000000; ++i)
-    {
-        if (sieve[i].first)
-            continue;
-
-        for (int j = (i * 2); j <= 20000000; j += i)
-            sieve[j].first = true, sieve[j].second = i;
-    }
-}
-
-void Sigma()
-{
-    sigma[1] = 1;
-    for (int i = 2; i <= 20000000; ++i)
-    {
-        if (!sieve[i].first)
-        {
-            sigma[i] = 2;
-            continue;
-        }
-        int x = i;
-        while (!(x % sieve[i].second))
-        {
-            ++sigma[i];
-            x /= sieve[i].second;
-        }
-        ++sigma[i];
-        sigma[i] *= sigma[x];
-    }
-}
-
-void CountRes()
-{
-    res[0] = false;
-    res[1] = true;
-    int last_lost = 0;
-    for (int i = 2; i <= 20000000; ++i)
-        res[i] = i - sigma[i] <= last_lost, last_lost += res[i] * i - last_lost * res[i];
-}
-
-inline void PreProcess()
-{
-    EraSieve();
-    Sigma();
-    CountRes();
-}
-
 void Solve()
 {
-    cin >> T;
-    PreProcess();
-    for (int qr = 0; qr < T; ++qr)
+    cin >> t;
+    for (int qr = 0; qr < t; ++qr)
     {
         Init();
-        cout << (res[N] ? "Ada\n" : "Vinit\n");
     }
 }
 
@@ -92,6 +37,7 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    Init();
     Solve();
 
     return 0;
