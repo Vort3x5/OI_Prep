c1b3b0a60752e797f8e65ca517b6d0739ae7e9b3
Vort3x5 <Faking.Games857@gmail.com>
2022-12-07 22:19:13 +0100

    Nie do konca

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -8,64 +8,61 @@ using namespace std;
 typedef long long ll;
 typedef pair <int, int> p_i;
 typedef vector <int> v_i;
+typedef int s32;
 
-int KMR[2000020][50], l, T;
+s32 Q, r, lvl;
+string s;
 
-string s, w;
-pair <p_i, int> p[2000020];
-v_i res;
+v_i qs;
+s32 KMR[1000010][21];
+vector <pair <s32, p_i>> buffs;
 
 void Init()
 {
-    cin >> s >> w;
-    s = w + s;
-    for (int i = 0; i < s.size(); ++i)
-    {
+    cin >> s >> Q;
+    r = log2(s.size()) + 1, lvl = 1;
+    qs.resize(Q);
+    buffs.resize(r + 1);
+    for (s32 &i : qs)
+        cin >> i;
+}
+
+void PreProcess()
+{
+    for (s32 i = 0; i < s.size(); ++i)
         KMR[i][0] = s[i] - 'a' + 1;
-    }
-    l = ceil(log2(s.size()));
-    for (int i = 1; i <= l; ++i)
+    for (s32 x = 1; x <= r; ++x)
     {
-        int j;
-        for (j = 0; j < s.size() - i; ++j)
-            p[j].first = {KMR[j][i - 1], KMR[j + (1 << (i - 1))][i - 1]}, p[j].second = j;
-        for (j = s.size() - i; j < s.size(); ++j)
-            p[j].first = { KMR[j][i - 1], -1 }, p[j].second = j;
-        sort(p, p + s.size());
-        p_i x = { -1, -1 };
-        int f = 0;
-        for (j = 0; j < s.size(); ++j)
+        for (s32 i = 0; i < s.size(); ++i)
         {
-            if (p[j].first.first != x.first || p[j].first.second != x.second)
-                x = p[j].first, ++f;
-            KMR[p[j].second][i] = f;
+            if (i + lvl >= s.size())
+                buffs[i] = make_pair(KMR[i][x - 1], make_pair(-1, i));
+            else
+                buffs[i] = make_pair(KMR[i][x - 1], make_pair(KMR[i + lvl][x - 1], i));
         }
+        sort(buffs.begin(), buffs.end());
+        p_i buff = {-1, -1};
+        s32 cnt = -1;
+        for (s32 i = 0; i < s.size(); ++i)
+        {
+            if (buff.first != buffs[i].first ||buff.second != buffs[i].second.first)
+            {
+                buff = {buffs[i].first, buffs[i].second.first};
+                ++cnt;
+            }
+            KMR[buffs[i].second.second][x] = cnt;
+        }
+        lvl *= 2;
     }
 }
 
 void Solve()
 {
-    cin >> T;
-    for (int qr = 0; qr < T; ++qr)
+    PreProcess();
+    for (s32 &qr : qs)
     {
-        Init();
-        int lw = log2(w.size());
-        int ls = 1 << lw;
-        for (int i = w.size(); i < s.size(); ++i)
-        {
-            if (KMR[0][lw] == KMR[i][lw] && KMR[w.size() - ls][lw] == KMR[i + w.size() - ls][lw])
-                res.pb(i - w.size() + 1);
-        }
-        if (res.empty())
-            cout << "Not Found\n";
-        else
-        {
-            cout << res.size() << '\n';
-            for (int i : res)
-                cout << i << ' ';
-            cout << '\n';
-        }
-        res.clear();
+        for (s32 i = buffs[qr - 1].second.second; i < s.size(); ++i)
+            cout << s[i];
         cout << '\n';
     }
 }
@@ -74,7 +71,8 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
+    
+    Init();
     Solve();
 
     return 0;
