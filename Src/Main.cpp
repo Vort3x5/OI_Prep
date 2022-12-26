78bd6f2b04fa0248a310bee2b324c226167eb042
Vort3x5 <Faking.Games857@gmail.com>
2022-12-26 14:11:44 +0100

    Setup

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -16,15 +16,25 @@ typedef pair <s64, s64> p64;
 typedef vector <int> v32;
 typedef vector <s64> v64;
 
-s32 n;
+s32 n, m;
+u64 dp[2000000000];
+
+v32 arr;
 
 void Init()
 {
     cin >> n;
+    arr.resize(n);
+    for (s32 &i : arr)
+        cin >> i;
+    cin >> m;
 }
 
 void Solve()
 {
+    for (s32 i = 1; i <= m; ++i)
+    {
+    }
 }
 
 int main()
