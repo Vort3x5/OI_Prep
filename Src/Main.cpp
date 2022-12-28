79318edf2a964e9b4c0be8b7b7f9e0f4b7ac3d29
Vort3x5 <Faking.Games857@gmail.com>
2022-12-28 13:41:58 +0100

    p

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -16,23 +16,31 @@ typedef pair <s64, s64> p64;
 typedef vector <int> v32;
 typedef vector <s64> v64;
 
-s32 n, m;
-u64 dp[2000000000];
+s32 n, m, v1[1000010], v2[1000010];
+u64 sum;
 
 v32 arr;
+unordered_map m1, m2;
 
 void Init()
 {
     cin >> n;
     arr.resize(n);
     for (s32 &i : arr)
+    {
         cin >> i;
+        sum += i;
+    }
     cin >> m;
 }
 
 void Solve()
 {
-    for (s32 i = 1; i <= m; ++i)
+    for (s32 i = 0; i < ( s32(log2(sum) + 1) / 2 ); ++i)
+    {
+    }
+    
+    for (s32 i = ( s32(log2(sum) + 1) / 2 ); i < (s32(log2(sum) + 1); ++i)
     {
     }
 }
